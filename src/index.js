b7a7a03af214af23ff4f4581706d1de6395aad2a
Konurbaeva <konurbaevamadina@gmail.com>
2022-03-02 22:20:21 +0100

    latest

--- src/index.js
+++ src/index.js
@@ -10,71 +10,54 @@ import lightBox from './js/lightBox';
  const searchApiService = new ImageApiService();
  let lightbox = new SimpleLightbox('.gallery a', { /* options */ });
 
-
- refs.searchForm.addEventListener('submit', onSearch);
- refs.loadMoreButton.addEventListener('click', onLoadMore);
- refs.galleryEl.addEventListener('click', onImageClick);
-
- function onSearch(event){
-  event.preventDefault();
-
-  searchApiService.query = refs.inputEl.value;
-
-/*   if (searchApiService.query === '' ) {
-    return  Notiflix.Notify.warning('Cannot be empty. Please provide your searching word!');
-  } */
-
-  if (searchApiService.query.trim() === '') {
-    return  Notiflix.Notify.warning('Cannot be empty. Please provide your searching word!');
-  }
-  searchApiService.resetPage();
-  searchApiService.fetchArticles();
- }
-
-
- function onImageClick(event){
-   event.preventDefault();
-
-   if(!event.currentTarget.classList.contains('card')) {
-     return;
+ const fetchPostsBtn = document.querySelector(".load-more");
+ const galleryList = document.querySelector(".gallery");
+ 
+ // Controls the group number
+ let page = 1;
+ // Controls the number of items in the group
+ let per_page = 5;
+ // In our case total number of pages is calculated on frontend
+ const totalPages = 100 / per_page;
+ 
+ fetchPostsBtn.addEventListener("click", () => {
+   // Check the end of the collection to display an alert
+   if (page > totalPages) {
+     console.log('page > totalPages')
    }
-
-   lightbox.open();
- }
-
- function onLoadMore(){
-   searchApiService.fetchArticles();
- }
-
- refs.loadMoreButton.addEventListener("click", () => {
-  fetchPosts()
-    .then((response) => renderPosts(response))
-    .catch((error) => console.log(error));
-});
-
-
+ 
+   fetchPosts()
+     .then((posts) => {
+       renderPosts(posts);
+       // Increase the group number
+       page += 1;
+ 
+       // Replace button text after first request
+       if (page > 1) {
+         fetchPostsBtn.textContent = "Fetch more posts";
+       }
+     })
+     .catch((error) => console.log(error));
+ });
+ 
  async function fetchPosts() {
-  const params = new URLSearchParams({
-    page: 1,
-    per_page: 5,
-  });
-
-  const response = await fetch(`${BASE_URL}/?key=${API_KEY}&q=dog&image_type=photo&orientation=horizontal&safesearch=true&${params}`);
+   const params = new URLSearchParams({
+     page: page,
+     per_page:per_page
+   });
+ 
+   const response = await fetch(`${BASE_URL}/?key=${API_KEY}&q=${refs.inputEl.value}&image_type=photo&orientation=horizontal&safesearch=true&${params}`);
    if (!response.ok) {
      throw new Error(response.status);
    }
    return await response.json();
-}
-
-function renderPosts(data) {
-  console.log('card was clicked!!')
-  console.log('DATA inside renderPosts!!')
-
-
-  const markup = data.hits
-    .map(({webformatURL,likes,views,comments, downloads}) => {
-      return `
-        <div class="flex-container">
+ }
+ 
+ function renderPosts(data) {
+   const markup = data.hits
+     .map(({ webformatURL, likes, views,comments,downloads}) => {
+       return `
+       <div class="flex-container">
         <div class="photo-card-item">
         <a href="${webformatURL}" target="_blank" rel="noopener noreferrer">
         <img src="${webformatURL}" alt="" loading="lazy" />
@@ -86,10 +69,8 @@ function renderPosts(data) {
             <b>Downloads </b>${downloads}
         </div>
       </div></div>
-        `;
-    })
-    .join("");
- // refs.galleryEl.innerHTML = markup;
-
-  refs.galleryEl.insertAdjacentHTML('beforeend', markup);
-}
+       `;
+     })
+     .join("");
+     galleryList.insertAdjacentHTML("beforeend", markup);
+ }
