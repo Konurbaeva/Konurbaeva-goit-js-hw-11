fc432ed2a314e616d286c4fd63be719e8b79fb80
Konurbaeva <konurbaevamadina@gmail.com>
2022-03-03 11:53:49 +0100

    migrating

--- src/js/get-refs.js
+++ src/js/get-refs.js
@@ -1,9 +1,9 @@
 export default function getRefs() {
-    return {
-      inputEl: document.querySelector('input[name="searchQuery"]'),
-      searchForm: document.querySelector('.search-form'),
-      button: document.querySelector('button[type="submit"]'),
-      galleryEl: document.querySelector('.gallery'),
-      loadMoreButton: document.querySelector('.load-more')
-    };
-  }
+  return {
+    inputEl: document.querySelector('input[name="searchQuery"]'),
+    searchForm: document.querySelector('.search-form'),
+    button: document.querySelector('button[type="submit"]'),
+    galleryEl: document.querySelector('.gallery'),
+    loadMoreButton: document.querySelector('.load-more')
+  };
+}
